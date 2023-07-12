8db6829bb3f25029059fa161b40221ef3bd0a833
devalenya1 <127754942+devalenya1@users.noreply.github.com>
2023-07-12 21:29:37 +0100

    Add files via upload

--- lib/repositories/coupon_repository.dart
+++ lib/repositories/coupon_repository.dart
@@ -8,12 +8,10 @@ import '../data_model/coupon_remove_response.dart';
 import '../helpers/shared_value_helper.dart';
 
 class CouponRepository {
-  Future<CouponApplyResponse> getCouponApplyResponse(
-      // String couponCode) async {
-      @required String coupon_code) async {
-    var post_body =
-        // jsonEncode({"user_id": "${user_id.$}", "code": "$couponCode"});
-        jsonEncode({"user_id": "${user_id.$}", "coupon_code": "$coupon_code"});
+  Future<CouponApplyResponse> getCouponApplyResponse(String couponCode) async {
+    var postBody = jsonEncode(
+        {"user_id": user_id.$.toString(), "coupon_code": couponCode});
+    // jsonEncode({"user_id": "${user_id.$}", "coupon_code": "$coupon_code"});
 
     Uri url = Uri.parse("${AppConfig.BASE_URL}/coupon-apply");
     final response = await http.post(url,
@@ -22,13 +20,13 @@ class CouponRepository {
           "Authorization": "Bearer ${access_token.$}",
           "App-Language": app_language.$
         },
-        body: post_body);
+        body: postBody);
 
     return couponApplyResponseFromJson(response.body);
   }
 
   Future<CouponRemoveResponse> getCouponRemoveResponse() async {
-    var post_body = jsonEncode({"user_id": "${user_id.$}"});
+    var postBody = jsonEncode({"user_id": user_id.$.toString()});
 
     Uri url = Uri.parse("${AppConfig.BASE_URL}/coupon-remove");
     final response = await http.post(url,
@@ -37,8 +35,41 @@ class CouponRepository {
           "Authorization": "Bearer ${access_token.$}",
           "App-Language": app_language.$
         },
-        body: post_body);
+        body: postBody);
 
     return couponRemoveResponseFromJson(response.body);
   }
+  // Future<CouponApplyResponse> getCouponApplyResponse(
+  //     // String couponCode) async {
+  //     @required String coupon_code) async {
+  //   var post_body =
+  //       jsonEncode({"user_id": user_id.$.toString(), "coupon_code": coupon_code});
+  //       // jsonEncode({"user_id": "${user_id.$}", "coupon_code": "$coupon_code"});
+
+  //   Uri url = Uri.parse("${AppConfig.BASE_URL}/coupon-apply");
+  //   final response = await http.post(url,
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //         "Authorization": "Bearer ${access_token.$}",
+  //         "App-Language": app_language.$
+  //       },
+  //       body: post_body);
+
+  //   return couponApplyResponseFromJson(response.body);
+  // }
+
+  // Future<CouponRemoveResponse> getCouponRemoveResponse() async {
+  //   var post_body = jsonEncode({"user_id": user_id.$.toString()});
+
+  //   Uri url = Uri.parse("${AppConfig.BASE_URL}/coupon-remove");
+  //   final response = await http.post(url,
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //         "Authorization": "Bearer ${access_token.$}",
+  //         "App-Language": app_language.$
+  //       },
+  //       body: post_body);
+
+  //   return couponRemoveResponseFromJson(response.body);
+  // }
 }
